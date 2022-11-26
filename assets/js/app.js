64b31ef470b3c2b0f72eccf454ea2cde4ed7d45a
YOSHITHA RATHNAYAKE <yoshithasbrathnayake@gmail.com>
2022-11-26 09:22:47 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+//function to load navbar and footer
+
+$(function () {
+    $("#footer").load("assets/content/static/footer.html");
+});
+
+$(function () {
+    $("#navbar").load("assets/content/static/navbar.html");
+});
+
