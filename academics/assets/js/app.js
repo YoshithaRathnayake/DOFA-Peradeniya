2dcc8e1bcbfab6be00397cc6ce92c87df6c25070
YOSHITHA RATHNAYAKE <yoshithasbrathnayake@gmail.com>
2022-12-20 16:29:31 +0530

    Add new codes to the javascript file
    Add new codes to the javascript file

--- academics/assets/js/app.js
+++ academics/assets/js/app.js
@@ -1,9 +1,24 @@
-//function to load navbar and footer
-
+// Function to load navbar and footer
 $(function () {
     $("#footer").load("../assets/content/static/footer.html");
 });
 
 $(function () {
     $("#navbar").load("../assets/content/static/navbar.html");
+});
+
+
+// Back to Top button 
+var btn = $('#backToTop');
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 80) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
 });
