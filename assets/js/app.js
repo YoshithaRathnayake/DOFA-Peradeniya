8dccf47fcb50753118d54b6f130bc692f8cac651
YOSHITHA RATHNAYAKE <yoshithasbrathnayake@gmail.com>
2022-12-20 16:00:06 +0530

    Add new codes to the app.js file
    Add new codes to the app.js file

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
-//function to load navbar and footer
-
+// Function to load navbar and footer
 $(function () {
     $("#footer").load("assets/content/static/footer.html");
 });
@@ -8,3 +7,38 @@ $(function () {
     $("#navbar").load("assets/content/static/navbar.html");
 });
 
+
+// Back to Top button 
+var btn = $('#backToTop');
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
+
+
+// Nav-Links on-click colour
+function home() {
+    document.getElementById("home").style.color = "#1eff8f";
+    document.getElementById("about").style.color = "white";
+    document.getElementById("contact").style.color = "white";
+}
+
+function about() {
+    document.getElementById("about").style.color = "#1eff8f";
+    document.getElementById("home").style.color = "white";
+    document.getElementById("contact").style.color = "white";
+}
+
+function contact() {
+    document.getElementById("contact").style.color = "#1eff8f";
+    document.getElementById("home").style.color = "white";
+    document.getElementById("about").style.color = "white";
+}
